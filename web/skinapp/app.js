eadde3d446b8c246e3a271a1aaf78fabf660abea
Kartik Singhal <kartiksinghal3.14@gmail.com>
2018-09-15 18:12:39 +0000

    added dependencies and integrated firebase

--- web/skinapp/app.js
+++ web/skinapp/app.js
@@ -1,13 +1,39 @@
-var express = require("express")
+var express = require("express"),
+bodyParser = require("body-parser"), 
+app = express(); 
 
-var app = express()
+app.set("view engine", "ejs"); 
+app.use(express.static("public"));
+var fs = require('fs');
+//====================================================
+//               FIREBASE CONFIG
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./key.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "htn2018-93d56.appspot.com",
+    databaseURL: "https://htn2018-93d56.firebaseio.com/"
+});
+
+var bucket = admin.storage().bucket();
+
+//====================================================
 
 
 app.get("/", function(req, res){
+    
+    
+    
     res.render("index.ejs"); 
 })
 
-
+app.get("/type/:type", function(req,res){
+    var type = req.params.type
+    res.render("index.ejs", {name: type})
+})
 
 
 
