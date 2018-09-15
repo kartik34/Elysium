1f57e52f81c2352dc8a7e453cd1ba9cd9cd3a45c
Kartik Singhal <kartiksinghal3.14@gmail.com>
2018-09-15 15:44:02 +0000

    basic project structure

---
+++ web/skinapp/app.js
@@ -0,0 +1,26 @@
+var express = require("express")
+
+var app = express()
+
+
+app.get("/", function(req, res){
+    res.render("index.ejs"); 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server is listening"); 
+})
